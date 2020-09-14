d191025f3238e51f8c0375892fc802eb4634cfc2
Happy singh <54718365+dicieous@users.noreply.github.com>
2020-09-15 00:36:58 +0530

    The whole project

---
+++ ARt/Assets/imagerecognition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class imagerecognition : MonoBehaviour
+{
+    private ARTrackedImageManager _artrackedimage;
+    private void Awake()
+    {
+        _artrackedimage = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    private void OnEnable()
+    {
+        _artrackedimage.trackedImagesChanged += OnImageChanged;
+    }
+
+    private void OnDisable()
+    {
+        _artrackedimage.trackedImagesChanged -= OnImageChanged;
+    }
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var trackedimage in args.added)
+        {
+            Debug.Log(trackedimage.name);
+        }
+    }
+}
